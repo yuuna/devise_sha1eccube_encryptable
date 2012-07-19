ac0b787f4b27244841adff7fa15a604f8d1a0532
Yuuna Kurita <yuuna.m@gmail.com>
2012-07-19 13:40:21 +0900

    fix for devise

--- lib/encryptors/sha1eccube.rb
+++ lib/encryptors/sha1eccube.rb
@@ -1,16 +1,18 @@
 require 'digest/sha1'
 
 module Devise
-  module Encryptors
-    # = Sha1eccube
-    # Sha1 with eccube format encrypter
-    class Sha1eccube < Base
-      class << self
-        #returns eccube encrypt with pepper
-        def digest(password, stretches, salt, pepper)
-          Digest::SHA1.hexdigest(password+":"+pepper)
-        end
+  module Encryptable
+    module Encryptors
+      # = Sha1eccube
+      # Sha1 with eccube format encrypter
+      class Sha1eccube < Base
+        class << self
+          #returns eccube encrypt with pepper
+          def digest(password, stretches, salt, pepper)
+            Digest::SHA1.hexdigest(password+":"+pepper)
+          end
         alias :encrypt :digest
+        end
       end
     end
   end
